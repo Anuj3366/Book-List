bbb29b161ea152a909b63a633af08453f50388f1
Anuj3366 <anujgarg3366@gmail.com>
2023-10-14 19:12:21 +0530

    ubutton done

--- src/common/components/alert/index.js
+++ src/common/components/alert/index.js
@@ -4,36 +4,36 @@ import React, { useEffect } from 'react';
 
 
 function UAlert({ message, show, toggleAlert }) {
-    useEffect(() => {
-        if (show) {
-            setTimeout(() => {
-                closeAlert(false);
-            }, 4000);
-        }
-    }, [show]);
-    return (
-        <>{show && (
-            <div aria-live="polite" aria-atomic="true" style="position: relative; min-height: 200px;">
-                <div class="toast" style="position: absolute; top: 0; right: 0;">
-                    <div class="toast-header">
-                        <img src="..." class="rounded mr-2" alt="..." />
-                        <strong class="mr-auto">{message}</strong>
-                        <small></small>
-                        <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close"
-                            onClick={() => {
-                                toggleAlert(false);
-                            }}>
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                    <div class="toast-body">
-                        Hello, world! This is a toast message.
-                    </div>
-                </div>
-            </div>
-        )
-        }
-        </>
-    );
+  useEffect(() => {
+    if (show) {
+      setTimeout(() => {
+        closeAlert(false);
+      }, 4000);
+    }
+  }, [show]);
+  return (
+    <>{show && (
+      <div aria-live="polite" aria-atomic="true" style="position: relative; min-height: 200px;">
+        <div class="toast" style="position: absolute; top: 0; right: 0;">
+          <div class="toast-header">
+            <img src="..." class="rounded mr-2" alt="..." />
+            <strong class="mr-auto">{message}</strong>
+            <small></small>
+            <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close"
+              onClick={() => {
+                toggleAlert(false);
+              }}>
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="toast-body">
+            Hello, world! This is a toast message.
+          </div>
+        </div>
+      </div>
+    )
+    }
+    </>
+  );
 }
 export default UAlert;
--- src/common/components/button/index.js
+++
@@ -1,8 +0,0 @@
-function UButton(props) {
-    return <span style={{ padding: "5px" }}>
-        <button className={`btn btn-${props.type}`}  onClick={props.onclick()}>
-            {props.children}
-        </button>
-    </span>
-}
-export default UButton;
